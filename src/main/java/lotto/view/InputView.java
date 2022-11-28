b862f6d6447286e6fb4260ff1b503236e79ed0c5
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-28 15:34:35 +0900

    Feat: Add lottogame.play() 로또 게임 시작을 알리는 기능 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class InputView {
+
+    // 로또 구입금액 입력
+
+    // 당첨번호 입력
+
+    // 보너스번호 입력
+}
---
+++ src/main/java/lotto/view/OutputVIew.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class OutputVIew {
+
+    // 로또 수량 및 번호 출력 (오름차순)
+
+    // 당첨 내역 출력
+
+    // 수익률 출력
+}
